ec60d058a2f5e6d4d7857591b7fb76b831429a91
outisa <savolainen.outi@gmail.com>
2019-07-23 14:40:44 +0300

    Tehtävä 1.3

--- Osa1/kurssitiedot/src/index.js
+++ Osa1/kurssitiedot/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Header = (props) => {
+    console.log(props)
     return (
         <div>
             <h1>{props.header1}</h1>
@@ -10,6 +11,7 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+    console.log(props)
     return (
         <div>
             <Part name={props.name1} amount={props.amount1}/>
@@ -30,6 +32,7 @@ const Total = (props) => {
 }
 
 const Part = (props) => {
+    console.log(props)
     return (
         <div>
             <p>{props.name} {props.amount}</p>
@@ -38,20 +41,25 @@ const Part = (props) => {
 }
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
-
+    const part1 = {
+        name:'Fundamentals of React',
+        exercises: 10,
+    }    
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7,
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14,
+    }    
     return (
         <div>
             <Header header1={course}/>
-            <Content name1={part1} amount1={exercises1} 
-                name2={part2} amount2={exercises2} 
-                name3={part3} amount3={exercises3} />
-            <Total total={exercises1 + exercises2 + exercises3}/>
+            <Content name1={part1.name} amount1={part1.exercises} 
+                name2={part2.name} amount2={part2.exercises} 
+                name3={part3.name} amount3={part3.exercises} />
+            <Total total={part1.exercises + part2.exercises + part3.exercises}/>
         </div>
     )
 }
